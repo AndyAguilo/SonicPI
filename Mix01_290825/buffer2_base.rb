75d1e5644a511af62dc1affdc49cd4540bccc1e8
Andy Serra <162577852+AndyAguilo@users.noreply.github.com>
2025-08-29 19:41:34 +0200

    Update buffer2_base.rb

--- Mix01_290825/buffer2_base.rb
+++ Mix01_290825/buffer2_base.rb
@@ -8,8 +8,8 @@ live_loop :bass do
       play note2, release: rrand(0.1, 0.3), amp: 2* get(:amp_2), pan: get(:pan_2), cutoff: rrand(40, 80)
     end
   end
-  human = rrand(-0.03, 0.03)
+  human = rrand(-0.01, 0.01)
   
   slp = choose([1, 2, 1, 1, 1])
-  sleep slp * (get(:sendA_2)+0.04) + human
+  sleep slp * (get(:sendA_2)+0.01) + human
 end
