c17e94317f6220349a09f5ba8ecce0bc10f90ccc
Andy Serra <162577852+AndyAguilo@users.noreply.github.com>
2025-09-16 09:26:40 +0200

    Update buffer3_bells.rb

--- Mix01_290825/buffer3_bells.rb
+++ Mix01_290825/buffer3_bells.rb
@@ -1,8 +1,17 @@
-live_loop :haunt do
+
+live_loop :rewind do
+  min_speed = 0.5 + get(:sendA_3)
+  max_speed = 2.3 + get(:sendB_3)
+  direction = [-1, 1].choose
+  speed = rrand(min_speed, max_speed)
+  f_rate1 = direction * speed
+  sustain_time = 1
+  release_time = 1
+  #
   use_bpm get(:master_bpm)
   if get(:part_3_on)
-    sample :perc_bell, rate: rrand(-2.5, 1.5), attack: 0.75, release: 0.75, amp: get(:amp_3)*rrand(0.5, 1.5), pan: get(:pan_3)
+    sample :perc_bell, rate: f_rate1, attack: 0.2, release: release_time, sustain: sustain_time, amp: get(:amp_3)*rrand(2.5, 4), pan: get(:pan_3)
   end
-  s = one_in(8) ? 2.5 : 1.5
-  sleep s
+  sleep sample_duration(:tabla_ghe6) + sustain_time + release_time / (f_rate1.abs + 0.01)
+  sleep one_in(8) ? 1 : 4
 end
